5746927fdda6896ce44e112bb885cd005c59420b
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-28 00:07:39 +0330

    Feature/sortArray completed

--- HW-7.js
+++ HW-7.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const sort = (...arr) => {
+  const empty = [];
+  const total = empty.concat(...arr);
+  total.sort((a, b) => b - a);
+  return total;
+};
+alert(sort([5, 6, 2], [3, 7, 1], [4, 8]));
+alert(sort([5, 6, 2, 4], [3, 7, 1]));
