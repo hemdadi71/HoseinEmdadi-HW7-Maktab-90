bd8ea205dcabcf2b412c59d370e4e3e920b2b09c
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-27 22:40:34 +0330

    answer of question 1 completed

--- HW-7.js
+++ HW-7.js
@@ -1,2 +1,19 @@
 'use strict';
 
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+const topSalary = obj => {
+  let maxsalaty = 0;
+  let maxName;
+  for (let [key, value] of Object.entries(obj)) {
+    if (maxsalaty < value) {
+      maxsalaty = value;
+      maxName = key;
+    }
+  }
+  return `maxSalary name is ${maxName}`;
+};
+alert(topSalary(salaries));
