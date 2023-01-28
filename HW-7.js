f0f1c83b866556dee0a42b00d8168b4ea7579933
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-28 17:57:14 +0330

    Feature/Unique_Function completed

--- HW-7.js
+++ HW-7.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const values = [
+  'Hare',
+  'Krishna',
+  'Hare',
+  'Krishna',
+  'Krishna',
+  'Krishna',
+  'Hare',
+  'Hare',
+  'John',
+  ':-O',
+  1,
+  1,
+  5,
+  2,
+  true,
+  true,
+  false,
+];
+
+const unique = arr => {
+  const filter = arr.filter((item, index) => arr.indexOf(item) === index);
+  return filter;
+};
+console.log(unique(values));
