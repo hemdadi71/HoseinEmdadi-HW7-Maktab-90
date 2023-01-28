d9af048ed4ca6dac6690b6439ff01df3d659f0b5
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-28 19:29:01 +0330

    Feature/Anagrams completed

--- HW-7.js
+++ HW-7.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const array = ['PAN', 'cheaters', 'hectares', 'nap', 'ear', 'ear', 'teachers'];
+
+const aclean = arr => {
+  const map = new Map();
+  for (const word of arr) {
+    const result = word.toLowerCase().split('').sort().join('');
+    map.set(result, word);
+  }
+  return Array.from(map.values());
+};
+console.log(aclean(array));
