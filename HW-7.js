71130558ade9690cd862857ab46356493411cf76
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-28 19:47:57 +0330

    Feature/map.keys completed

--- HW-7.js
+++ HW-7.js
@@ -1,2 +1,8 @@
 'use strict';
 
+let map = new Map();
+map.set('name', 'John');
+let keys = map.keys(); // this is an object and we can't use array method for this
+let array = Array.from(keys); //first turn keys to array
+array.push('more'); //then use push to add more to array from keys
+console.log(array);
