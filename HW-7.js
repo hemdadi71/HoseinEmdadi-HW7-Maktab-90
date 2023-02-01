2aa9b4cc0c6f59e90f272ab1cc89ad4e42880546
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-01 23:21:52 +0330

    Feature/formValidation completed

--- HW-7.js
+++ HW-7.js
@@ -8,7 +8,11 @@ const retypePassword = document.getElementById('retypePassword');
 const passMatch = document.getElementById('passMatch');
 const email = document.getElementById('email');
 const emailMatch = document.getElementById('emailMatch');
-const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const passPatternValid = document.getElementById('passPatternValid');
+const emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const passwordPattern =
+  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+// ..................................................................................................
 
 const handleCheckValidation = e => {
   e.preventDefault();
@@ -19,57 +23,86 @@ const handleCheckValidation = e => {
       const labelEl = input.parentElement;
       const errorEl = labelEl.childNodes[3];
       errorEl.classList.add('active');
+      errorEl.innerHTML = `this field is required!`;
       input.style.border = '1px solid red';
+
+      e.currentTarget.childNodes[3].style.gap = `1.7rem`;
+      e.currentTarget.childNodes[5].style.top = `56rem`;
     }
 
     input.addEventListener('keypress', () => {
       if (input.value) {
         if (input.parentElement.childNodes[3].classList.contains('active')) {
+          input.parentElement.childNodes[3].innerHTML = `this field is required!`;
           input.parentElement.childNodes[3].classList.remove('active');
-          input.style.border = '1px solid green';
+          input.style.border = 'none';
         }
       }
     });
   });
-
   handleEmailValidation();
   handlePasswordMatch();
   if (form.dataset.isValid === '1') {
     form.reset();
     passMatch.classList.remove('opacity');
-    input.forEach(item => (item.style.border = '1px solid transparent'));
+    passPatternValid.classList.remove('opacity');
   }
 };
+// ...........................................................................................
 
 const handlePasswordMatch = () => {
-  if (password.value !== retypePassword.value) {
-    passMatch.innerHTML = `Password Not Match`;
-    retypePassword.style.border = '1px solid red';
-    passMatch.classList.add('opacity');
-    passMatch.style.color = 'red';
-    form.dataset.isValid === '0';
-  } else if (password.value === '') {
-    passMatch.innerHTML = 'this field is required!';
-    passMatch.style.color = 'red';
-  } else {
-    passMatch.innerHTML = `Password Matched`;
-    passMatch.style.color = 'green';
-    passMatch.classList.add('opacity');
-    retypePassword.style.border = '1px solid green';
+  if (password.value !== '') {
+    if (passwordPattern.test(password.value)) {
+      passPatternValid.innerHTML = `Password is Strong`;
+      passPatternValid.style.color = 'green';
+      passPatternValid.classList.add('opacity');
+      password.style.border = `none`;
+    } else {
+      form.dataset.isValid = `0`;
+      passPatternValid.innerHTML = `Password is Weak`;
+      passPatternValid.style.color = `red`;
+      passPatternValid.classList.add('opacity');
+      password.style.border = `1px solid red`;
+    }
+    if (retypePassword.value !== '') {
+      if (password.value !== retypePassword.value) {
+        form.dataset.isValid = `0`;
+        passMatch.innerHTML = `Password Not Match`;
+        retypePassword.style.border = '1px solid red';
+        passMatch.classList.add('opacity');
+        passMatch.style.color = 'red';
+        form.dataset.isValid === '0';
+      } else {
+        passMatch.innerHTML = `Password Matched`;
+        passMatch.style.color = 'green';
+        passMatch.classList.add('opacity');
+        retypePassword.style.border = 'none';
+      }
+    }
   }
 };
+// ..................................................................................
 
 const handleEmailValidation = () => {
-  if (email.value !== '' && pattern.test(email.value)) {
+  if (email.value !== '' && emailPattern.test(email.value)) {
     emailMatch.innerHTML = 'Example: john@gmail.com';
-    email.style.border = '1px solid green';
+    email.style.border = 'none';
     emailMatch.style.opacity = '0';
-  } else if (email.value !== '' && !pattern.test(email.value)) {
+  } else if (email.value !== '' && !emailPattern.test(email.value)) {
+    form.dataset.isValid = `0`;
     emailMatch.innerHTML = 'Example: john@gmail.com';
     form.dataset.isValid = '0';
     email.style.border = '1px solid red';
     emailMatch.style.opacity = '1';
   }
 };
+// ......................................................................................
+
+const handleChangeEvent = () => {
+  handleEmailValidation();
+  handlePasswordMatch();
+};
+// ..........................................................................................
 
 form.addEventListener('submit', handleCheckValidation);
+input.forEach(item => item.addEventListener('change', handleChangeEvent));
